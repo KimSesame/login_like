403f677752650e8abd2837105b96cf0ba45c8df8
SOJAEMIN0906 <sojaemin1314@gmail.com>
2024-04-09 14:43:30 +0900

    Remove None used scripts
    사용하지 않는 스크립트들을 삭제하고 스크립트의 위치를 옮겼습니다

--- Assets/02.Scripts/SkillDeck.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public class SkillDeck : MonoBehaviour
-{
-    [SerializeField]
-    GameObject[] skillSets;
-}
--- Assets/02.Scripts/Skills/SkillDetail.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class SkillDetail : MonoBehaviour
-{
-    public abstract void UseSkill(Target target, byte posX, byte posY);
-}
