442bba9ecb1b9a108a5f05d952662169af7952af
Vin0625 <kwan000625@gmail.com>
2024-04-10 18:40:20 +0900

    upload rail

---
+++ Assets/skillrail/SkillIcon.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillIcon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 9f;
+    private Rigidbody2D rb;
+    private bool canMove = true;
+
+    private bool click =false;
+    void Start()
+    {   
+         rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove){
+        rb.velocity= new Vector2(-1,0)*speed;
+        }else{
+            rb.velocity = Vector2.zero;
+            Vector3 currentPosition = transform.position;
+            transform.position = currentPosition;
+        }
+        
+    }
+    
+    private void OnMouseDown() {//클릭시
+    if(click==true){
+        Destroy(gameObject);
+        SkillSpawn.cur--;
+    }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // "Wall" 태그를 가진 오브젝트와 충돌 시 멈춤
+        if (collision.gameObject.tag == "Wall")
+        {
+            canMove = false;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        // "Wall" 태그를 가진 오브젝트와 충돌이 끝나면 다시 이동 가능
+        if (collision.gameObject.tag == "Wall")
+        {
+            canMove = true;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other) {
+        click=true;
+    }
+}
---
+++ Assets/skillrail/SkillSpawn.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class SkillSpawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject skill1;
+    public GameObject skill2;
+    public GameObject skill3;
+    public GameObject skill4;
+    public GameObject skill5;
+    public GameObject skill6;
+    public GameObject skill7;
+
+    public List<GameObject> slist = new List<GameObject>();
+    List<int> numbers = new List<int> { 0,1, 2, 3, 4, 5, 6 };
+    float max=8;
+    public static float cur=0;
+
+    public int sufflecount=0;
+    float timer=0;
+    void Start()
+    {
+        ShuffleList(numbers);
+        slist.Add(skill1);
+        slist.Add(skill2);
+        slist.Add(skill3);
+        slist.Add(skill4);
+        slist.Add(skill5);
+        slist.Add(skill6);
+        slist.Add(skill7);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        //숫자를 랜덤으로 뽑기
+        timer+=Time.deltaTime;
+        if(cur<=max&&timer>1){
+            if(sufflecount==7){
+                sufflecount=0;
+                ShuffleList(numbers);
+            }
+            GameObject newskill = Instantiate(slist[numbers[sufflecount]]);
+            sufflecount++;
+            newskill.transform.position=new Vector3(10,-3f,0);
+            cur++;
+            timer=0;
+        }
+    }
+
+    void ShuffleList<T>(List<T> list)
+    {
+        System.Random rng = new System.Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = rng.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
